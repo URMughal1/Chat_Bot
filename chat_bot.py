8c7d971ea5f74476dd5a33f20d1de49904ea56b3
URMughal1 <33569327+URMughal1@users.noreply.github.com>
2019-10-14 09:09:21 +0200

    closest mach function implemented with additional functionality

--- chat_bot.py
+++ chat_bot.py
@@ -1,14 +1,20 @@
+'''
+Author:Umer Rehman
+Last Time changed: 14-10-2019
+'''
+
 import sys
 from datetime import datetime
 from difflib import get_close_matches
 
-
+# Closest word match function 
 def closeMatches(word,patterns):
-    #patterns=(patterns.title()).replace(" ","")
-    print(patterns)
-    print(word)
-    print(get_close_matches(word, patterns))
-    return str(get_close_matches(word, patterns,n=3))
+    try:
+        sw=get_close_matches(word, patterns, n=1)[0]
+    except IndexError:
+        sw=""
+    return str(sw)
+
 # Making Time stemped File name
 dateTimeObj = datetime.now()
 timestampStr = dateTimeObj.strftime("%d-%b-%Y_(%H-%M-%S)")
@@ -49,7 +55,8 @@ def Truck_Information(Number):
     print("Chatbot: What is the "+Number+" brand Name?")
     Brand_Name = closeMatches(Check_isEmpty("Chatbot: What is the "+Number+" brand Name?",input("Customer: ")),Brands_Name)
     write_to_text.write("Customer: "+ Brand_Name + " \n ")
-    while(not Brands_Name):
+
+    while(len(Brand_Name)==0):
         write_to_text.write("Chatbot: What is the " + Number + " brand Name?[Entered Brand Not Found]" + " \n ")
         print("Chatbot: What is the " + Number + " brand Name?[Entered Brand Not Found]")
         Brand_Name = closeMatches(input("Customer: "),Brands_Name)
@@ -72,29 +79,83 @@ def Truck_Information(Number):
         C_input = Check_isEmpty("Chatbot: What model is it?",input("Customer: "))
         write_to_text.write("Customer: " + C_input + " \n ")
 
+        write_to_text.write("Chatbot: What is the engine size of Truck?" + " \n ")
+        print("Chatbot: What is the engine size of Truck?")
+        C_input = Check_isEmpty("Chatbot: What is the engine size of Truck?", input("Customer: "))
+        write_to_text.write("Customer: " + C_input + " \n ")
+
+        write_to_text.write("Chatbot: How many Axles are in this Truck?" + " \n ")
+        print("Chatbot: How many Axles are in this Truck?")
+        C_input = Check_isEmpty("Chatbot: How many Axles are in this Truck?", input("Customer: "))
+        write_to_text.write("Customer: " + C_input + " \n ")
+
     if ("no" in C_input.lower() or "n" in C_input.lower()):
         j = 1
         while (j <= Truck_Quantity):
             if (j == 1):
                 write_to_text.write("Chatbot: what is the 1st Truck model?" + " \n ")
-                print("Chatbot: what is the 1st Truck model?")
-                C_input =Check_isEmpty("Chatbot: what is the 1st Truck model?",input("Customer: "))
+                print("Chatbot: What is the 1st Truck model?")
+                C_input =Check_isEmpty("Chatbot: What is the 1st Truck model?",input("Customer: "))
                 write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: What is the engine size of 1st Truck?" + " \n ")
+                print("Chatbot: What is the engine size of 1st Truck?")
+                C_input = Check_isEmpty("Chatbot: What is the engine size of 1st Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: How many Axles are in 1st Truck?" + " \n ")
+                print("Chatbot: How many Axles are in 1st Truck?")
+                C_input = Check_isEmpty("Chatbot: How many Axles are in 1st Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
             elif (j == 2):
-                write_to_text.write("Chatbot: what is the 2nd Truck model?" + " \n ")
-                print("Chatbot: what is the 2nd Truck model?")
-                C_input = input("Customer: ")
+                write_to_text.write("Chatbot: What is the 2nd Truck model?" + " \n ")
+                print("Chatbot: What is the 2nd Truck model?")
+                C_input = Check_isEmpty("Chatbot: What is the 2nd Truck model?",input("Customer: "))
                 write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: What is the engine size of 2nd Truck?" + " \n ")
+                print("Chatbot: What is the engine size of 2nd Truck?")
+                C_input = Check_isEmpty("Chatbot: What is the engine size of 2nd Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: How many Axles are in 2nd Truck?" + " \n ")
+                print("Chatbot: How many Axles are in 2nd Truck?")
+                C_input = Check_isEmpty("Chatbot: How many Axles are in 2nd Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
             elif (j == 3):
-                write_to_text.write("Chatbot: what is the 3nrd Truck model?" + " \n ")
-                print("Chatbot: what is the 3nrd Truck model?")
-                C_input = Check_isEmpty("Chatbot: what is the 3nrd Truck model?",input("Customer: "))
+                write_to_text.write("Chatbot: What is the 3rd Truck model?" + " \n ")
+                print("Chatbot: What is the 3nrd Truck model?")
+                C_input = Check_isEmpty("Chatbot: What is the 3rd Truck model?",input("Customer: "))
                 write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: What is the engine size of 3rd Truck?" + " \n ")
+                print("Chatbot: What is the engine size of 3rd Truck?")
+                C_input = Check_isEmpty("Chatbot: What is the engine size of 3rd Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: How many Axles are in 3rd Truck?" + " \n ")
+                print("Chatbot: How many Axles are in 3rd Truck?")
+                C_input = Check_isEmpty("Chatbot: How many Axles are in 3rd Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
             elif (i > 3):
-                write_to_text.write("Chatbot: what is the " + str(j) + "th Truck model?" + " \n ")
-                print("Chatbot: what is the " + str(j) + "th Truck model?")
-                C_input = Check_isEmpty("Chatbot: what is the " + str(j) + "th Truck model?",input("Customer: "))
+                write_to_text.write("Chatbot: What is the " + str(j) + "th Truck model?" + " \n ")
+                print("Chatbot: What is the " + str(j) + "th Truck model?")
+                C_input = Check_isEmpty("Chatbot: What is the " + str(j) + "th Truck model?",input("Customer: "))
                 write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: What is the engine size of " + str(j) + "th Truck?" + " \n ")
+                print("Chatbot: What is the engine size of " + str(j) + "th Truck?")
+                C_input = Check_isEmpty("Chatbot: What is the engine size of " + str(j) + "th Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
+                write_to_text.write("Chatbot: How many Axles are in " + str(j) + "th Truck?" + " \n ")
+                print("Chatbot: How many Axles are in " + str(j) + "th Truck?")
+                C_input = Check_isEmpty("Chatbot: How many Axles are in " + str(j) + "th Truck?", input("Customer: "))
+                write_to_text.write("Customer: " + C_input + " \n ")
+
             j = j + 1
     elif("yes" in C_input.lower() or "y" in C_input.lower()):
         if(Truck_Quantity==1):
@@ -102,10 +163,30 @@ def Truck_Information(Number):
             print("Chatbot: What model is it?")
             C_input = Check_isEmpty("Chatbot: What model is it?",input("Customer: "))
             write_to_text.write("Customer: " + C_input + " \n ")
+
+            write_to_text.write("Chatbot: What is the engine size of Truck?" + " \n ")
+            print("Chatbot: What is the engine size of Truck?")
+            C_input = Check_isEmpty("Chatbot: What is the engine size of Truck?", input("Customer: "))
+            write_to_text.write("Customer: " + C_input + " \n ")
+
+            write_to_text.write("Chatbot: How many Axles are in this Truck?" + " \n ")
+            print("Chatbot: How many Axles are in this Truck?")
+            C_input = Check_isEmpty("Chatbot: How many Axles are in this Truck?", input("Customer: "))
+            write_to_text.write("Customer: " + C_input + " \n ")
         else:
             write_to_text.write("Chatbot: What model are they?" + " \n ")
             print("Chatbot: What model are they?")
             C_input =Check_isEmpty("Chatbot: What model are they?",input("Customer: "))
+            write_to_text.write("Customer: " + C_input + " \n ")
+
+            write_to_text.write("Chatbot: What is the engine size of These Trucks?" + " \n ")
+            print("Chatbot: What is the engine size of These Trucks?")
+            C_input = Check_isEmpty("Chatbot: What is the engine size of These Trucks?", input("Customer: "))
+            write_to_text.write("Customer: " + C_input + " \n ")
+
+            write_to_text.write("Chatbot: Number of Axles per truck?" + " \n ")
+            print("Chatbot: Number of Axles per truck?")
+            C_input = Check_isEmpty("Chatbot: Number of Axles per truck?", input("Customer: "))
             write_to_text.write("Customer: " + C_input + " \n ")
 
 #Starting Point of the Program
@@ -114,9 +195,9 @@ print("Chatbot: Hello, Your Full Name Please")
 Customer_Name =Check_isEmpty( "Chatbot: Hello, Your Full Name Please",input("Customer: "))
 write_to_text.write("Customer: " + Customer_Name + " \n ")
 
-write_to_text.write("Chatbot: Hi " + Customer_Name + " what\'s the name of your company?" + " \n ")
-print("Chatbot: Hi " + Customer_Name + " what\'s the name of your company?")
-C_input = Check_isEmpty("Chatbot: Hi " + Customer_Name + " what\'s the name of your company?",input("Customer: "))
+write_to_text.write("Chatbot: Hi " + Customer_Name + " What\'s the name of your company?" + " \n ")
+print("Chatbot: Hi " + Customer_Name + " What\'s the name of your company?")
+C_input = Check_isEmpty("Chatbot: Hi " + Customer_Name + " What\'s the name of your company?",input("Customer: "))
 write_to_text.write("Customer: " + C_input + " \n ")
 
 write_to_text.write("Chatbot: Do you own Trucks?[Yes/No]" + " \n ")
